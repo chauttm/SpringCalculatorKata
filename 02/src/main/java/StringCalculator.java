697cde9f04740d4a0176a9072903d7fadb334014
Chau Tran <chauttm.vnu@gmail.com>
2013-06-01 21:22:13 +0700

    green

--- 02/src/main/java/StringCalculator.java
+++ 02/src/main/java/StringCalculator.java
@@ -7,6 +7,8 @@
  */
 public class StringCalculator {
     public static int add(String numbers) {
-        return 0;
+        if (numbers==null) return 0;
+        if (numbers.isEmpty()) return 0;
+        return Integer.parseInt(numbers);
     }
 }
