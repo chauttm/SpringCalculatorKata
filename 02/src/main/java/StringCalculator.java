a6b73079256c8254ba5a7b65d0fe7f6dc5f862e4
Chau Tran <chauttm.vnu@gmail.com>
2013-06-01 22:00:25 +0700

    green

--- 02/src/main/java/StringCalculator.java
+++ 02/src/main/java/StringCalculator.java
@@ -7,18 +7,20 @@ public class StringCalculator {
         int startOfNumbers = 0;
         if (numbers.startsWith("//")) {
             startOfNumbers = numbers.indexOf('\n') + 1;
-            String newDelimiter = numbers.substring(2,3);
+            String newDelimiter = numbers.substring(2, 3);
             delimiterPattern += "|" + newDelimiter;
         }
         String[] addends = numbers.substring(startOfNumbers).split(delimiterPattern);
         int sum = 0;
+        StringBuffer errorMessage = new StringBuffer();
         for (String number : addends) {
             sum += Integer.parseInt(number);
             if (0>Integer.parseInt(number)) {
-                throw new RuntimeException("negative not allowed: -1");
+                errorMessage.append(" " + number + ",");
             }
         }
-
+        if (errorMessage.length() > 0)
+            throw new RuntimeException("negative not allowed:" + errorMessage.toString());
         return sum     ;
 
     }
