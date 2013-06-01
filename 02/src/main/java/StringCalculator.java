bd04ed05c996372a13a179c2bcb800f417b2a20e
Chau Tran <chauttm.vnu@gmail.com>
2013-06-01 21:42:19 +0700

    green

--- 02/src/main/java/StringCalculator.java
+++ 02/src/main/java/StringCalculator.java
@@ -3,7 +3,14 @@ public class StringCalculator {
         if (numbers==null) return 0;
         if (numbers.isEmpty()) return 0;
 
-        String[] addends = numbers.split(",|\n");
+        String delimiterPattern = ",|\n";
+        int startOfNumbers = 0;
+        if (numbers.startsWith("//")) {
+            startOfNumbers = numbers.indexOf('\n') + 1;
+            String newDelimiter = numbers.substring(2,3);
+            delimiterPattern += "|" + newDelimiter;
+        }
+        String[] addends = numbers.substring(startOfNumbers).split(delimiterPattern);
         int sum = 0;
         for (String number : addends) {
             sum += Integer.parseInt(number);
