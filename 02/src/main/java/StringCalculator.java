626f2e54d7a3ba10d46beea763cd77411f300f1a
Chau Tran <chauttm.vnu@gmail.com>
2013-06-01 22:10:02 +0700

    refactor

--- 02/src/main/java/StringCalculator.java
+++ 02/src/main/java/StringCalculator.java
@@ -1,27 +1,37 @@
 public class StringCalculator {
+
+    private static String defaultDelimiterPattern = ",|\n";
+
     public static int add(String numbers) {
         if (numbers==null) return 0;
         if (numbers.isEmpty()) return 0;
 
-        String delimiterPattern = ",|\n";
-        int startOfNumbers = 0;
-        if (numbers.startsWith("//")) {
-            startOfNumbers = numbers.indexOf('\n') + 1;
-            String newDelimiter = numbers.substring(2, 3);
-            delimiterPattern += "|" + newDelimiter;
-        }
+        String delimiterPattern = getDelimiterPattern(numbers);
+        int startOfNumbers = getStartOfNumbers(numbers);
         String[] addends = numbers.substring(startOfNumbers).split(delimiterPattern);
+
         int sum = 0;
         StringBuffer errorMessage = new StringBuffer();
         for (String number : addends) {
             sum += Integer.parseInt(number);
-            if (0>Integer.parseInt(number)) {
+            if (0 > Integer.parseInt(number)) {
                 errorMessage.append(" " + number + ",");
             }
         }
         if (errorMessage.length() > 0)
             throw new RuntimeException("negative not allowed:" + errorMessage.toString());
-        return sum     ;
+        return sum;
+    }
 
+    private static String getDelimiterPattern(String numbers) {
+        if (numbers.startsWith("//"))
+            return (defaultDelimiterPattern + "|" + numbers.substring(2, 3));
+        return defaultDelimiterPattern;
+    }
+
+    private static int getStartOfNumbers(String numbers) {
+        if (numbers.startsWith("//"))
+            return numbers.indexOf('\n') + 1;
+        return 0;
     }
 }
